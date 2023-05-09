6d7cb605fd906bbd8200776a7307aca31056a802
anime-sh01 <animesh@leapsquarelabs.com>
2023-05-09 17:23:48 +0530

    added getters and setters for using dio API service

--- lib/common/services/dio_api_service.dart
+++ lib/common/services/dio_api_service.dart
@@ -1,1 +1,62 @@
-class DioApiService{}
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+
+class DioApiService {
+  late String _baseUrl;
+
+  String get baseUrl => _baseUrl;
+
+  set baseUrl(String value) {
+    _baseUrl = value;
+    initializeDio();
+  }
+
+  late final Dio _dio;
+
+  void initializeDio() {
+    _dio = Dio(
+      BaseOptions(
+        baseUrl: _baseUrl,
+        connectTimeout: const Duration(seconds: 5),
+        receiveTimeout: const Duration(seconds: 10),
+        headers: {
+          HttpHeaders.userAgentHeader: 'dio',
+          'api': '1.0.0',
+        },
+        contentType: Headers.jsonContentType,
+        responseType: ResponseType.plain,
+      ),
+    );
+  }
+
+  Future<Response<dynamic>> get(
+    String path,
+    Map<String, dynamic>? queryParameters,
+  ) {
+    return _dio.get(path, queryParameters: queryParameters);
+  }
+
+  Future<Response<dynamic>> post(
+    String path,
+    dynamic data,
+    Map<String, dynamic>? queryParameters,
+  ) {
+    return _dio.post(path, data: data, queryParameters: queryParameters);
+  }
+
+  Future<Response<dynamic>> put(
+    String path,
+    dynamic data,
+    Map<String, dynamic>? queryParameters,
+  ) {
+    return _dio.put(path, data: data, queryParameters: queryParameters);
+  }
+
+  Future<Response<dynamic>> delete(
+    String path,
+    Map<String, dynamic>? queryParameters,
+  ) {
+    return _dio.delete(path, queryParameters: queryParameters);
+  }
+}
--- lib/counter/view/counter_page.dart
+++ lib/counter/view/counter_page.dart
@@ -1,6 +1,10 @@
 // import 'package:flutter/foundation.dart';
+// ignore_for_file: cascade_invocations
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:workroom_flutter_app/common/services/dio_api_service.dart';
 // import 'package:hive/hive.dart';
 import 'package:workroom_flutter_app/counter/counter.dart';
 // import 'package:workroom_flutter_app/l10n/l10n.dart';
@@ -40,6 +44,38 @@ class CounterView extends StatelessWidget {
             //     print(box.get('name'));
             //   }
             // },
+
+            // ! just for testing dio.
+            // onPressed: () async {
+            //   try {
+            //     final dioApiService = DioApiService();
+            //     dioApiService.baseUrl = 'https://httpbin.org/';
+            // await dioApiService.get('get', null).then((value) {
+            //   if (kDebugMode) {
+            //     print(value.data);
+            //     print(value.statusCode);
+            //   }
+            // });
+            // await dioApiService
+            //     .post(
+            //   'post',
+            //   {
+            //     'id': 8,
+            //     'info': {'name': 'wendux', 'age': 25}
+            //   },
+            //   null,
+            // )
+            //     .then((value) {
+            //   if (kDebugMode) {
+            //     print(value.statusCode);
+            //     print(value.data);
+            //   }
+            // });
+            // } catch (e) {
+            //   if (kDebugMode) {
+            //     print(e);
+            //   }
+            // }
             child: const Icon(Icons.add),
           ),
           const SizedBox(height: 8),
