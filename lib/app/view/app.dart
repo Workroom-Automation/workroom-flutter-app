7b6c51a1e1a25d13b283fc5350f977b062b6161c
Babycobra <rohan.a@captainfresh.in>
2023-05-11 10:56:26 +0530

    Adding navigationn service

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:workroom_flutter_app/common/services/connection_service/connection_service.dart';
+import 'package:workroom_flutter_app/common/services/navigation_service/navigation_service.dart';
 import 'package:workroom_flutter_app/core/di/injection.dart';
 import 'package:workroom_flutter_app/counter/counter.dart';
 import 'package:workroom_flutter_app/l10n/l10n.dart';
@@ -9,11 +10,13 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final navigatorKey = getIt<NavigationService>().myNavigatorKey;
     return StreamBuilder<Object>(
       stream: getIt<ConnectionService>().connectionChange,
       builder: (context, snapshot) {
         if (snapshot.hasData && snapshot.data == ConnectionStatus.good) {
           return MaterialApp(
+            navigatorKey: navigatorKey,
             theme: ThemeData(
               appBarTheme: const AppBarTheme(color: Color(0xFF13B9FF)),
               colorScheme: ColorScheme.fromSwatch(
@@ -26,6 +29,7 @@ class App extends StatelessWidget {
           );
         }
         return MaterialApp(
+          navigatorKey: navigatorKey,
           theme: ThemeData(
             appBarTheme:
                 const AppBarTheme(color: Color.fromARGB(255, 15, 16, 16)),
---
+++ lib/common/services/navigation_service/navigation_service.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:injectable/injectable.dart';
+import 'package:workroom_flutter_app/common/services/logger_service/logger_service.dart';
+
+@singleton
+class NavigationService {
+  final GlobalKey<NavigatorState> myNavigatorKey = GlobalKey<NavigatorState>();
+  BuildContext? get currentContext => myNavigatorKey.currentContext;
+  State? get currentState => myNavigatorKey.currentState;
+  Future<dynamic>? pushNamed(String routeName, {dynamic arguments}) {
+    return myNavigatorKey.currentState
+        ?.pushNamed(routeName, arguments: arguments);
+  }
+
+  Future<dynamic>? pushReplacementNamed(String routeName, {dynamic arguments}) {
+    return myNavigatorKey.currentState?.pushReplacementNamed(
+      routeName,
+      arguments: arguments,
+    );
+  }
+
+  Future<dynamic>? pushAndRemoveUntil(
+    Route<dynamic> route,
+    bool Function(Route<dynamic>) predicate,
+  ) {
+    return myNavigatorKey.currentState?.pushAndRemoveUntil(route, predicate);
+  }
+
+  Future<dynamic>? pushNamedAndRemoveUntil(
+    String routeName,
+    bool Function(Route<dynamic>) predicate, {
+    dynamic arguments,
+  }) {
+    return myNavigatorKey.currentState?.pushNamedAndRemoveUntil(
+      routeName,
+      predicate,
+      arguments: arguments,
+    );
+  }
+
+  void pop() {
+    return myNavigatorKey.currentState?.pop();
+  }
+
+  void killApp() {
+    SystemChannels.platform.invokeMethod('SystemNavigator.pop');
+  }
+
+  String? getCurrentScreenName() {
+    String? currentScreenName;
+    myNavigatorKey.currentState?.popUntil((route) {
+      currentScreenName = route.settings.name;
+      AppLogger.printLog('checkCurrentPath -- $currentScreenName');
+      return true;
+    });
+    return currentScreenName ?? '';
+  }
+}
